b6848a69bdba3c4f8ed228957662e2fa83f7ed0a
GILAD-PC\Gilad <leifmangilad@gmail.com>
2017-01-10 02:03:07 +0200

    Added basic argparse

--- main.py
+++ main.py
@@ -24,7 +24,31 @@ def main(pdf_paths, result_path):
 
 
 def parse_arguments():
-    return [os.path.join("test", x) for x in os.listdir("test")], "result.pdf"
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-f', "--folder", dest="pdfs_folder", type=str,
+                    help='folder of pdf files, sorted')
+    group.add_argument('-s', "--pdfs", type=str, nargs="+",
+                    help='folder of pdf files, sorted')
+    parser.add_argument('result_file', type=str,
+                    help='path to result of merge')
+    args = parser.parse_args()
+
+    def dir_dir(name):
+        return [os.path.join(name, x) for x in os.listdir(name)]
+
+    pdfs = []
+    if args.pdfs_folder is not None:
+        pdfs_folder = os.path.abspath(args.pdfs_folder)
+        if not os.path.isdir(pdfs_folder):
+            raise Exception("folder must be a folder")
+        else:
+            pdfs = dir_dir(pdfs_folder)
+    else:
+        pdfs = [os.path.abspath(x) for x in args.pdfs]
+    if not any(pdfs):
+        raise Exception("None pdf files have been found.")
+    return pdfs, args.result_file
 
 if __name__ == '__main__':
     main(*parse_arguments())
